0b38b68d4b42e90c16ae549eedb491bccec69a9f
roboemily <31740786+roboemily@users.noreply.github.com>
2017-09-14 15:19:49 -0600

    Start server

---
+++ assignment_1/server/server.py
@@ -0,0 +1,24 @@
+
+
+########Globals
+#port parameter on which client can connect
+port = None
+#file w/ setup of your board
+file = None
+
+
+def main(port, file):
+#when fire: send message to other server, get response back, update own board based on that info
+
+def headers(self):
+    self.send_response(200)
+    self.send_header('Content-type', 'text/html')
+    self.end_headers()
+
+def GET(self):
+    self.headers()
+    self.wfile.write("")
+
+def POST(self):
+    self.headers()
+    self.wfile.write("")
