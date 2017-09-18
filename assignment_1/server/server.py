4fb3ba286f80ba048dd9922edb5ed6282fd62034
roboemily <31740786+roboemily@users.noreply.github.com>
2017-09-18 16:56:58 -0600

    Error free with command line args

--- assignment_1/server/server.py
+++ assignment_1/server/server.py
@@ -1,15 +1,14 @@
-import http.server
-import BaseHTTPRequestHandler
-import HTTPServer
+import httplib
+import sys
+import urllib
 import sys
 import array
-
-class server(BaseHTTPRequestHandler):
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 ########Globals
 #port parameter on which client can connect
 port = None
 #file w/ setup of your board
-file = None
+board = None
 #x and y coordinates of hit
 x = None
 y = None
@@ -21,83 +20,90 @@ bhit = 0
 rhit = 0
 shit = 0
 dhit = 0
+FAILURE = "\033[1;31;40m"
+DEF_C = "\033[0;37;40m"
+class server(BaseHTTPRequestHandler):
 
-#text file
-
-
-def main():
-
-    run()
-#when fire: send message to other server, get response back, update own board based on that info
-
-def headers(self, code):
-    self.send_response(code)
-    self.send_header('Content-type', 'text/html')
-    self.end_headers()
-
-def do_GET(self, code):
-    self.headers(code)
-    self.wfile.write(bytes(file, "utf8"))
-
-def do_POST(self, message, code):
-    self.headers(code)
-    self.wfile.write(message)
-
-def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    httpd.serve_forever()
-
-def checkInput(x, y):
-    if x < 0 || x > 9 || y < 0 || y > 9:
-        do_Post("", 404)
+    #text file
 
 
+    def main():
+        if(len(sys.argv) != 3):
+            print(FAILURE + 'incorrect number of arguments!' + DEF_C)
+            sys.exit(1)
+        port = sys.argv[1]
+        board = sys.argv[2]
+    #when fire: send message to other server, get response back, update own board based on that info
 
-def checkBoard():
-    f = open(file, 'r')
-    row = file.readline(x-1)
-    cell = row[y-1]
-    if cell == 'C':
-        chit++
-        if chit == 5:
-            do_Post("hit=1&sink=C", 200)
-            #carrier sunk
+    def headers(self, code):
+        self.send_response(code)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self, code):
+        self.headers(code)
+        self.wfile.write(bytes(board, "utf8"))
+
+    def do_POST(self, message, code):
+        self.headers(code)
+        self.wfile.write(message)
+
+    def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
+        server_address = ('', port)
+        httpd = server_class(server_address, handler_class)
+        httpd.serve_forever()
+
+    def checkInput(x, y):
+        if x < 0 | x > 9 | y < 0 | y > 9:
+            do_Post("", 404)
+
+
+    def checkBoard():
+        f = open(board, 'r')
+        row = file.readline(x-1)
+        cell = row[y-1]
+        if cell == 'C':
+            chit = chit +1
+            if chit == 5:
+                do_Post("hit=1&sink=C", 200)
+                #carrier sunk
+            else:
+                do_Post("hit=1", 200)
+                #carrier hit
+        elif cell == 'B':
+            bhit = bhit +1
+            if bhit == 4:
+                do_Post("hit=1&sink=B", 200)
+                #battleship sunk
+            else:
+                do_Post("hit=1", 200)
+                #battleship hit
+        elif cell == 'R':
+            rhit = rhit +1
+            if rhit == 3:
+                do_Post("hit=1&sink=R", 200)
+                #cruiser sunk
+            else:
+                do_Post("hit=1", 200)
+                #cruiser hit
+        elif cell == 'S':
+            shit = shit +1
+            if shit == 3:
+                do_Post("hit=1&sink=S", 200)
+                #sub sunk
+            else:
+                do_Post("hit=1", 200)
+                #sub hit
+        elif cell == 'D':
+            dhit = dhit +1
+            if dhit == 2:
+                do_Post("hit=1&sink=D", 200)
+                #destroyer sunk
+            else:
+                do_Post("hit=1", 200)
+                #destroyer hit
         else:
-            do_Post("hit=1", 200)
-            #carrier hit
-    else if cell == 'B':
-        bhit++
-        if bhit == 4:
-            do_Post("hit=1&sink=B", 200)
-            #battleship sunk
-        else:
-            do_Post("hit=1", 200)
-            #battleship hit
-    else if cell == 'R':
-        rhit++
-        if rhit == 3:
-            do_Post("hit=1&sink=R", 200)
-            #cruiser sunk
-        else:
-            do_Post("hit=1", 200)
-            #cruiser hit
-    else if cell == 'S':
-        shit++
-        if shit == 3:
-            do_Post("hit=1&sink=S", 200)
-            #sub sunk
-        else:
-            do_Post("hit=1", 200)
-            #sub hit
-    else if cell == 'D':
-        dhit++
-        if dhit == 2:
-            do_Post("hit=1&sink=D", 200)
-            #destroyer sunk
-        else:
-            do_Post("hit=1", 200)
-            #destroyer hit
-    else:
-        do_Post("hit=0", 200)
-        #missed
+            do_Post("hit=0", 200)
+            #missed
+
+    main()
