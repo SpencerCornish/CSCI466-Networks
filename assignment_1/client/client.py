9f605697e1c893b5eabe099ee8a40e13b1c3afb6
SpencerCornish <spencerjcornish@gmail.com>
2017-09-18 16:53:56 -0600

    Add Clientside handlers

--- assignment_1/client/client.py
+++ assignment_1/client/client.py
@@ -2,7 +2,7 @@
 import httplib
 import sys
 import urllib
-
+import os
 
 
 ############ Fun text formatters
@@ -11,50 +11,89 @@ IMPORTANT = "\033[1;33;40m"
 FAILURE = "\033[1;31;40m"
 SUCCESS = "\033[1;32;40m"
 DEF_C = "\033[0;37;40m"
-############ Globals
+
+def printC(color, message):
+    print(color + message + DEF_C)
 
 
-LOCAL_IP = "127.0.0.1"
-# IP Address
-ip = None
-# Port
-port = None
+# Invoked when out of bounds coords were sent
+def outOfBounds(message):
+    printC(IMPORTANT, 'ERROR (Code 404): Coordinates are out of bounds! Message as follows:')
+    print(message)
+    sys.exit(2)
 
-conn = None
+# Invoked when coords were already fired upon
+def alreadyFired(message):
+    printC(IMPORTANT, 'ERROR (Code 410): Coordinates have already been fired upon! Message as follows: ')
+    print(message)
+    sys.exit(2)
 
-############ Function Definitions
-def main():
-    setup()
+# Invoked when invalid data formatting was used to communicate
+def badRequest(message):
+    printC(FAILURE, 'ERROR (Code 400): Bad request. Ensure query data is of a proper format. Message as follows: ')
+    print(message)
+    sys.exit(2)
+
+# Invoked when a hit or miss occurred
+def processValidResponse(message):
+    printC(SUCCESS, 'Good things happened!')
+
+
+
+
+
+def validateFile():
+    ## True if we hava a board
+    if(os.path.isfile('enemy_board.txt')):
+        return True
+    else:
+        ## True if we have the board template
+        if(os.path.isfile('enemy_board_template.txt')):
+            printC(IMPORTANT, 'generating new enemy board tracker file...')
+        else:
+            printC(FAILURE, 'No Board or template found!')
+
 
 
 # The Setup parses the IP and port and establishes a connection
-def setup():
-    print TITLE + '\n\n**********Battleship Client**********\n\n' + DEF_C
-
+if __name__ == '__main__':
 
     if(len(sys.argv) != 5):
-        print(FAILURE + 'incorrect number of arguments!' + DEF_C)
+        printC(FAILURE, '\nIncorrect number of arguments!')
+        print ('Example: python client.py [IP] [PORT] [X coordinate] [Y coordinate] \n')
         sys.exit(1)
+
+    printC(SUCCESS,'\n\n**********Battleship Client**********\n\n')
+
+    # Find the files we need right away, or make them
+    validateFile()
+
+
+    # Cache off the IP, port, and coordinates for readability's sake
     ip = sys.argv[1]
     port = sys.argv[2]
-    paramList = {'x': sys.argv[3], 'y' : sys.argv[4]}
-    encodedUrl = '/?' + urllib.urlencode(paramList)
-    print encodedUrl
-    # if(xCord > BOARD_WIDTH || xCord < 0)
-    #     print(FAILURE + 'X input must be in between 0 and ' + BOARD_WIDTH + DEF_C)
-    #     sys.exit(1)
-    # if(yCord > BOARD_WIDTH || xCord < 0)
-    #     print(FAILURE + 'Y input must be in between 0 and' + BOARD_LENGTH +  DEF_C)
-    #     sys.exit(1)
+    xCord = sys.argv[3]
+    yCord = sys.argv[4]
 
 
-    print 'Attempting to estabish a connection to '+ encodedUrl
+    paramList = {'x': xCord, 'y' : yCord}
+    encodedUrl = '/fire?' + urllib.urlencode(paramList)
+
+    print 'Attempting to POST to '+ ip + ':' + port +  encodedUrl
     conn = httplib.HTTPConnection(host=ip, port=port)
-    conn.request("GET", encodedUrl)
-    r = conn.getresponse()
-    print(SUCCESS + r.read() + DEF_C)
+    conn.request("POST", encodedUrl)
 
+    response = conn.getresponse()
 
+    if(response.status == 404):
+        outOfBounds(response.read())
+    elif(response.status == 410):
+        alreadyFired(response.read())
+    elif(response.status == 400):
+        badRequest(response.read())
+    else:
+        processValidResponse(response.read())
 
-############ Main
-main()
+    # Close the connection and exit cleanly
+    conn.close()
+    sys.exit(0)
