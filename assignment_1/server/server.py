a1b6e0648aab7149584e3522cdb48c46efe00ae7
roboemily <31740786+roboemily@users.noreply.github.com>
2017-09-18 19:20:07 -0600

    Add methods for POST and GET

--- assignment_1/server/server.py
+++ assignment_1/server/server.py
@@ -3,17 +3,20 @@ import sys
 import urllib
 import sys
 import array
+import socket
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 ########Globals
 #port parameter on which client can connect
 port = None
 #file w/ setup of your board
 board = None
+oboard = None
 #x and y coordinates of hit
 x = None
 y = None
 #message to client whether it was a hit or miss
-
+code = None
+message = None
 #number of hits each ship has recieved
 chit = 0
 bhit = 0
@@ -22,17 +25,8 @@ shit = 0
 dhit = 0
 FAILURE = "\033[1;31;40m"
 DEF_C = "\033[0;37;40m"
-class server(BaseHTTPRequestHandler):
+class Server(BaseHTTPRequestHandler):
 
-    #text file
-
-
-    if __name__ == '__main__':
-        if(len(sys.argv) != 3):
-            print(FAILURE + 'incorrect number of arguments!' + DEF_C)
-            sys.exit(1)
-        port = sys.argv[1]
-        board = sys.argv[2]
     #when fire: send message to other server, get response back, update own board based on that info
 
     def headers(self, code):
@@ -45,8 +39,12 @@ class server(BaseHTTPRequestHandler):
         self.wfile.write(bytes(board, "utf8"))
 
     def do_POST(self, message, code):
+        coor = urllib.parse.parse_qs(self.path)
+        x = coor[0]
+        y = coor[1]
+        checkInput(x, y)
         self.headers(code)
-        self.wfile.write(message)
+        self.wfile.write(message.encode())
 
     def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
         server_address = ('', port)
@@ -55,53 +53,94 @@ class server(BaseHTTPRequestHandler):
 
     def checkInput(x, y):
         if x < 0 | x > 9 | y < 0 | y > 9:
-            do_Post("", 404)
-
+            message = "HTTP Not Found"
+            code = 404
+            return message, code
+        elif oboard[x-1][y-1] == 'X' | oboard[x-1][y-1] == '.':
+            message = "HTTP Gone"
+            code = 410
+            return message, code
+        else:
+            checkBoard()
 
     def checkBoard():
-        f = open(board, 'r')
-        row = file.readline(x-1)
-        cell = row[y-1]
+        cell = oboard[x-1][y-1]
         if cell == 'C':
             chit = chit +1
+            oboard[x-1][y-1] = 'X'
             if chit == 5:
-                do_Post("hit=1&sink=C", 200)
+                message = "hit=1&sink=C"
+                code = 200
                 #carrier sunk
             else:
-                do_Post("hit=1", 200)
+                message = "hit=1"
+                code = 200
                 #carrier hit
         elif cell == 'B':
             bhit = bhit +1
+            oboard[x-1][y-1] = 'X'
             if bhit == 4:
-                do_Post("hit=1&sink=B", 200)
+                message = "hit=1&sink=B"
+                code = 200
                 #battleship sunk
             else:
-                do_Post("hit=1", 200)
+                message = "hit=1"
+                code = 200
                 #battleship hit
         elif cell == 'R':
             rhit = rhit +1
+            oboard[x-1][y-1] = 'X'
             if rhit == 3:
-                do_Post("hit=1&sink=R", 200)
+                message = "hit=1&sink=R"
+                code = 200
                 #cruiser sunk
             else:
-                do_Post("hit=1", 200)
+                message = "hit=1"
+                code = 200
                 #cruiser hit
         elif cell == 'S':
             shit = shit +1
+            oboard[x-1][y-1] = 'X'
             if shit == 3:
-                do_Post("hit=1&sink=S", 200)
+                message = "hit=1&sink=S"
+                code = 200
                 #sub sunk
             else:
-                do_Post("hit=1", 200)
+                message = "hit=1"
+                code = 200
                 #sub hit
         elif cell == 'D':
             dhit = dhit +1
+            oboard[x-1][y-1] = 'X'
             if dhit == 2:
-                do_Post("hit=1&sink=D", 200)
+                message = "hit=1&sink=D"
+                code = 200
                 #destroyer sunk
             else:
-                do_Post("hit=1", 200)
+                message = "hit=1"
+                code = 200
                 #destroyer hit
         else:
-            do_Post("hit=0", 200)
+            oboard[x-1][y-1] = '.'
+            message = "hit=0"
+            code = 200
+        return message, code
             #missed
+
+    def show_board(which_board):
+        g = open(which_board, 'r')
+        b = g.read()
+        message = b
+        return message
+
+    if __name__ == '__main__':
+        if(len(sys.argv) != 3):
+            print(FAILURE + 'incorrect number of arguments!' + DEF_C)
+            sys.exit(1)
+        port = sys.argv[1]
+        board = sys.argv[2]
+        oboard = []
+        f = open(board, 'r')
+        for line in f:
+            oboard.append(list(line))
+        run(port)
