e559db1e8493c7f2f926e3b55bdb9afcb338dde4
Spencer Cornish <spencerjcornish@gmail.com>
2017-11-06 13:09:19 -0700

    adds sim 3 files

--- assignment_3/simulation_3.py
+++ assignment_3/simulation_3.py
@@ -3,8 +3,8 @@ Created on Oct 12, 2016
 
 @author: mwitt_000
 '''
-import network_2
-import link_2
+import network_3
+import link_3
 import threading
 from time import sleep
 
@@ -13,26 +13,25 @@ router_queue_size = 0 #0 means unlimited
 simulation_time = 5 #give the network sufficient time to transfer all packets before quitting
 
 if __name__ == '__main__':
-    object_L = [] #keeps track of objects, so we can kill their threads
+    object_L = [] # keeps track of objects, so we can kill their threads
 
-    #create network nodes
-    client_1 = network_2.Host(1)
+    # create network nodes
+    client_1 = network_3.Host(1)
     object_L.append(client_1)
 
-    client_2 = network_2.Host(2)
+    client_2 = network_3.Host(2)
     object_L.append(client_2)
 
-    server_1 = network_2.Host(3)
+    server_1 = network_3.Host(3)
     object_L.append(server_1)
 
-    server_2 = network_2.Host(4)
+    server_2 = network_3.Host(4)
     object_L.append(server_2)
 
-
-    router_a = network_2.Router(name='A', intf_count=4, max_queue_size=router_queue_size)
-    router_b = network_2.Router(name='B', intf_count=2, max_queue_size=router_queue_size)
-    router_c = network_2.Router(name='C', intf_count=2, max_queue_size=router_queue_size)
-    router_d = network_2.Router(name='D', intf_count=4, max_queue_size=router_queue_size)
+    router_a = network_3.Router(name='A', intf_count=4, max_queue_size=router_queue_size)
+    router_b = network_3.Router(name='B', intf_count=2, max_queue_size=router_queue_size)
+    router_c = network_3.Router(name='C', intf_count=2, max_queue_size=router_queue_size)
+    router_d = network_3.Router(name='D', intf_count=4, max_queue_size=router_queue_size)
 
     object_L.append(router_a)
     object_L.append(router_b)
@@ -40,26 +39,26 @@ if __name__ == '__main__':
     object_L.append(router_d)
 
     # Create a Link Layer to keep track of links between network nodes
-    link_layer = link_2.LinkLayer()
+    link_layer = link_3.LinkLayer()
     object_L.append(link_layer)
 
     # Links for client 1-2
-    link_layer.add_link(link_2.Link(client_1, 0, router_a, 0, 50))
-    link_layer.add_link(link_2.Link(client_2, 1, router_a, 1, 50))
+    link_layer.add_link(link_3.Link(client_1, 0, router_a, 0, 50))
+    link_layer.add_link(link_3.Link(client_2, 1, router_a, 1, 50))
 
     # Links for Router A
-    link_layer.add_link(link_2.Link(router_a, 2, router_b, 0, 50))
-    link_layer.add_link(link_2.Link(router_a, 3, router_c, 0, 50))
+    link_layer.add_link(link_3.Link(router_a, 2, router_b, 0, 50))
+    link_layer.add_link(link_3.Link(router_a, 3, router_c, 0, 50))
 
     # Links for Router B
-    link_layer.add_link(link_2.Link(router_b, 1, router_d, 0, 50))
+    link_layer.add_link(link_3.Link(router_b, 1, router_d, 0, 50))
 
     # Links for Router C
-    link_layer.add_link(link_2.Link(router_c, 1, router_d, 1, 50))
+    link_layer.add_link(link_3.Link(router_c, 1, router_d, 1, 50))
 
     # Links for Router D
-    link_layer.add_link(link_2.Link(router_d, 2, server_1, 0, 50))
-    link_layer.add_link(link_2.Link(router_d, 3, server_2, 0, 50))
+    link_layer.add_link(link_3.Link(router_d, 2, server_1, 0, 50))
+    link_layer.add_link(link_3.Link(router_d, 3, server_2, 0, 50))
 
 
     #start all the objects
@@ -88,7 +87,7 @@ if __name__ == '__main__':
 
     #create some send events
     for i in range(1):
-        client.udt_send(2, 'Turmoil has engulfed the Galactic Republic. The taxation of trade routes to .... %d' % i)
+        client_1.udt_send(2, 'Turmoil has engulfed the Galactic Republic. The taxation of trade routes to .... %d' % i)
 
 
 
