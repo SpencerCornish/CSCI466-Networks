95cdbd633c21baa85fe3fa41652bb93c807b0c37
Spencer Cornish <spencerjcornish@gmail.com>
2017-11-05 20:31:52 -0700

    Adds part 3 simulation file

---
+++ assignment_3/simulation_3.py
@@ -0,0 +1,108 @@
+'''
+Created on Oct 12, 2016
+
+@author: mwitt_000
+'''
+import network_2
+import link_2
+import threading
+from time import sleep
+
+##configuration parameters
+router_queue_size = 0 #0 means unlimited
+simulation_time = 5 #give the network sufficient time to transfer all packets before quitting
+
+if __name__ == '__main__':
+    object_L = [] #keeps track of objects, so we can kill their threads
+
+    #create network nodes
+    client_1 = network_2.Host(1)
+    object_L.append(client_1)
+
+    client_2 = network_2.Host(2)
+    object_L.append(client_2)
+
+    server_1 = network_2.Host(3)
+    object_L.append(server_1)
+
+    server_2 = network_2.Host(4)
+    object_L.append(server_2)
+
+
+    router_a = network_2.Router(name='A', intf_count=4, max_queue_size=router_queue_size)
+    router_b = network_2.Router(name='B', intf_count=2, max_queue_size=router_queue_size)
+    router_c = network_2.Router(name='C', intf_count=2, max_queue_size=router_queue_size)
+    router_d = network_2.Router(name='D', intf_count=4, max_queue_size=router_queue_size)
+
+    object_L.append(router_a)
+    object_L.append(router_b)
+    object_L.append(router_c)
+    object_L.append(router_d)
+
+    # Create a Link Layer to keep track of links between network nodes
+    link_layer = link_2.LinkLayer()
+    object_L.append(link_layer)
+
+    # Links for client 1-2
+    link_layer.add_link(link_2.Link(client_1, 0, router_a, 0, 50))
+    link_layer.add_link(link_2.Link(client_2, 1, router_a, 1, 50))
+
+    # Links for Router A
+    link_layer.add_link(link_2.Link(router_a, 2, router_b, 0, 50))
+    link_layer.add_link(link_2.Link(router_a, 3, router_c, 0, 50))
+
+    # Links for Router B
+    link_layer.add_link(link_2.Link(router_b, 1, router_d, 0, 50))
+
+    # Links for Router C
+    link_layer.add_link(link_2.Link(router_c, 1, router_d, 1, 50))
+
+    # Links for Router D
+    link_layer.add_link(link_2.Link(router_d, 2, server_1, 0, 50))
+    link_layer.add_link(link_2.Link(router_d, 3, server_2, 0, 50))
+
+
+    #start all the objects
+    thread_L = []
+
+    # Start client 1-2
+    thread_L.append(threading.Thread(name=client_1.__str__(), target=client_1.run))
+    thread_L.append(threading.Thread(name=client_2.__str__(), target=client_2.run))
+
+    # Start Routers
+    thread_L.append(threading.Thread(name=router_a.__str__(), target=router_a.run))
+    thread_L.append(threading.Thread(name=router_b.__str__(), target=router_b.run))
+    thread_L.append(threading.Thread(name=router_c.__str__(), target=router_c.run))
+    thread_L.append(threading.Thread(name=router_d.__str__(), target=router_d.run))
+
+    # Start Servers
+    thread_L.append(threading.Thread(name=server_1.__str__(), target=server_1.run))
+    thread_L.append(threading.Thread(name=server_2.__str__(), target=server_2.run))
+
+    # Start Link Layer
+    thread_L.append(threading.Thread(name="Network", target=link_layer.run))
+
+    for t in thread_L:
+        t.start()
+
+
+    #create some send events
+    for i in range(1):
+        client.udt_send(2, 'Turmoil has engulfed the Galactic Republic. The taxation of trade routes to .... %d' % i)
+
+
+
+    #give the network sufficient time to transfer all packets before quitting
+    sleep(simulation_time)
+
+    #join all threads
+    for o in object_L:
+        o.stop = True
+    for t in thread_L:
+        t.join()
+
+    print("All simulation threads joined")
+
+
+
+# writes to host periodically
